501e7ca808430442cbbcfb7c66d5dc31b26f094a
argen muratov <ibragim@MacBook-Pro-argen.local>
2024-10-22 16:26:08 +0300

    feat: Сделал controller для получения фильма по url

--- src/film/film.controller.ts
+++ src/film/film.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { FilmService } from './film.service';
 
 @Controller('film')
-export class FilmController {}
+export class FilmController {
+    constructor(
+        private readonly filmService:FilmService
+    ){}
+    @Get(":url")
+    async getFilmByUrl(@Param("url") url:string){
+        const film = await this.filmService.getFilmByUrl(url)
+        if(!film){
+            throw new BadRequestException()
+        }
+        return film
+    }
+}
