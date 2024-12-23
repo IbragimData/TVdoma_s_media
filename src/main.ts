49e0cb4ecbc11cfac18c2481007492881b55fbf0
IbragimData <ibragimsaydazimov@gmail.com>
2024-12-23 09:46:58 +0300

    f2

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from "express"
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.json({limit: "50mb"}))
-  app.use(express.urlencoded({limit: "50mb", extended: true}))
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true
-  }))
+  app.use(express.json({ limit: '5gb' }));
+  app.use(express.urlencoded({ limit: '5gb', extended: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
-  .build();
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
-  app.setGlobalPrefix("api")
-  app.enableCors()
+  app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(5004);
 }
 bootstrap();
