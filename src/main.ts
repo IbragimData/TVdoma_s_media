6c787b536fdf0bf9eb638956f2e8157cca1b7ce7
IbragimData <ibragimsaydazimov@gmail.com>
2024-12-23 12:33:19 +0300

    f4

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,34 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
-import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Используем встроенные методы Express для загрузки больших файлов
+  app.use(express.json({ limit: '5gb' }));
   app.use(express.urlencoded({ limit: '5gb', extended: true }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-  app.use(bodyParser.json({ limit: '5gb' }));
-  app.use(bodyParser.urlencoded({ limit: '5gb', extended: true }));
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
+  
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
+
   app.setGlobalPrefix('api');
   app.enableCors();
+
   await app.listen(5004);
 }
+
 bootstrap();
