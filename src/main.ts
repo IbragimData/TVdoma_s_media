fe26e373fc60bf2abdc00d321f6c16a38f3cd00e
IbragimData <ibragimsaydazimov@gmail.com>
2024-12-23 17:10:13 +0300

    feat: f5

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,23 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
-import * as bodyParser from "body-parser"
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Используем встроенные методы Express для загрузки больших файлов
-  app.use(bodyParser.json({limit: "5gb"}))
+  app.use(bodyParser.json({ limit: '5gb' }));
   app.use(bodyParser.urlencoded({ limit: '5gb', extended: true }));
+
+  app.use((req, res, next) => {
+    res.setTimeout(3600000, () => {
+      // Установите 1 час (3600 секунд) на выполнение запроса
+      console.log('Request has timed out');
+      res.sendStatus(408); // Возвращаем код ошибки 408 (Request Timeout)
+    });
+    next();
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
